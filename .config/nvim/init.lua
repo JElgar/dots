a8245e6be7c161a1ef699d8f5be65ca40859e529
jelgar <jamesnelgar@gmail.com>
2022-08-20 22:00:36 +0100

    Treesitter and neotest config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,42 +1,51 @@
 vim.g.mapleader = ' '
 
 local function keymap(mode, key, command)
-	local opts = {noremap = true, silent = true}
-	vim.api.nvim_set_keymap(mode, key, command, opts)
+    local opts = { noremap = true, silent = true }
+    vim.api.nvim_set_keymap(mode, key, command, opts)
 end
 
 local packer_bootstrap = require('packer-setup').setup()
 
--- A config returns a packages and setup function 
-local configs = {'configs.lsp', 'configs.tree', 'configs.theme', 'configs.telescope', 'configs.flutter', 'configs.dap', 'configs.test'}
+-- Reload Nvim on init.lua file save
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost init.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+-- A config returns a packages and setup function
+local configs = { 'configs.lsp', 'configs.treesitter', 'configs.test', 'configs.tree', 'configs.theme', 'configs.telescope',
+    'configs.flutter', 'configs.dap' }
 
 require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	
-	-- Setup all the configs
-	for _, config in pairs(configs) do
-		local mod = require(config)
-		if not mod then
-			print(config .. ' config not found')
-		end
-		if mod.packages then
-			mod.packages(use)
-		end
-		if mod.setup then
-			mod.setup()
-		end
-		if mod.keybindings then
-			mod.keybindings(keymap)
-		end
-	end
+    use 'wbthomason/packer.nvim'
 
-	-- Extra dependencies
-	-- TMUX navigation
-	use 'christoomey/vim-tmux-navigator'
-	
-	if packer_bootstrap then
-    		require('packer').sync()
-  	end
+    -- Setup all the configs
+    for _, config in pairs(configs) do
+        local mod = require(config)
+        if not mod then
+            print(config .. ' config not found')
+        end
+        if mod.packages then
+            mod.packages(use)
+        end
+        if mod.setup then
+            mod.setup()
+        end
+        if mod.keybindings then
+            mod.keybindings(keymap)
+        end
+    end
+
+    -- Extra dependencies
+    -- TMUX navigation
+    use 'christoomey/vim-tmux-navigator'
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
 
 keymap('i', 'jk', '<ESC>')
--- .config/nvim/lua/configs/test.lua
+++ .config/nvim/lua/configs/test.lua
@@ -1,58 +1,36 @@
 return {
-	packages = function(use)
-		use({
-			'rcarriga/neotest',
-  			requires = {
-  			  'haydenmeade/neotest-jest',
-  			  'nvim-neotest/neotest-python',
-  			},
-  			config = function()
-				require('neotest').setup({
-					adapters = {
-					  require('neotest-python'),
-  			    	  require('neotest-jest')({
-  			    	    jestCommand = "npm test --",
-  			    	    jestConfigFile = "custom.jest.config.ts",
-  			    	    env = { CI = true },
-  			    	    cwd = function(path)
-							return vim.fn.getcwd()
-  			    	    end,
-  			    	  }),
-  			    	}
-				})
-			end
-		})
-	end,
-	setup = function()
-		-- local neotest = require("neotest")
-
-		-- neotest.setup({
-		-- 	adapters = {
-		-- 		require("neotest-python")({
-		-- 			dap = { justMyCode = false },
-		--     	}),
-		-- 		require("neotest-plenary"),
-		-- 		require("neotest-rust")
-		-- 	},
-		-- 	require('neotest-jest')({
-		-- 		jestCommand = "npm test --",
-        -- 	  	jestConfigFile = "custom.jest.config.ts",
-        -- 	  	env = { CI = true },
-        -- 	  	cwd = function(path)
-		-- 			return vim.fn.getcwd()
-        -- 	  	end,
-        -- 	}),
-		-- })
-
-		-- local adapters = neotest.run.adapters()
-		-- for a in pairs(adapters) do
-		-- 	print(a)
-		-- end
-
-		-- local count = 0
-		-- for _ in pairs(adapters) do count = count + 1 end
-		-- print(count)
-	end,
-	keybindings = function (bindkey)
-	end,
+    packages = function(use)
+        use({
+            "nvim-neotest/neotest",
+            requires = {
+                "nvim-lua/plenary.nvim",
+                "nvim-treesitter/nvim-treesitter",
+                "antoinemadec/FixCursorHold.nvim",
+                "haydenmeade/neotest-jest",
+                "nvim-neotest/neotest-python",
+                "rouge8/neotest-rust",
+            },
+        })
+    end,
+    setup = function()
+        require("neotest").setup({
+            adapters = {
+                require("neotest-python"),
+                require("neotest-rust"),
+                require("neotest-jest")({
+                	jestCommand = "npm test --",
+                  	jestConfigFile = "custom.jest.config.ts",
+                  	env = { CI = true },
+                  	cwd = function(path)
+                		return vim.fn.getcwd()
+                  	end,
+                }),
+            },
+        })
+    end,
+    keybindings = function(bindkey)
+        bindkey("", "<leader>tr", "<cmd>lua require('neotest').run.run()<cr>")
+        bindkey("", "<leader>td", "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>")
+        bindkey("", "<leader>ts", "<cmd>lua require('neotest').summary.open()<cr>")
+    end,
 }
